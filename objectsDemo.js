888c087623329766e21c5acd073afe6ecb48c1b2
Max Johansen <macsj200@gmail.com>
2014-06-26 18:12:01 -0600

    add objectsDemo

--- objectsDemo.js
+++ objectsDemo.js
@@ -5,3 +5,36 @@
 //inheritance skeleton zombie
 
 //prototypal
+
+/**
+ * This is how things inherit in javascript
+ *
+**/
+
+//make a Villain constructor
+function Villain(damage, health){
+    this.damage = damage;
+    this.health = health;
+}
+
+//make a Skeleton (a villain)
+function Skeleton(numberOfBones){
+    this.damage = 5;
+    this.health = 7;
+
+    this.numberOfBones = numberOfBones;
+}
+
+Skeleton.prototype = new Villain();
+
+
+//make a Zombie (a villain)
+function Zombie(hungerLevel){
+    this.damage = 2;
+    this.health = 20;
+
+    this.hungerLevel = hungerLevel;
+}
+
+Zombie.prototype = new Villain();
+
